6955088ca94cb9a31e5e305b077e1d04703b2d19
Marc-Antoine Bouchard Marceau <marcantoine.bouchardm@gmail.com>
2015-09-13 17:32:20 -0400

    Setup behat and phpunit test, change the readme.md file

---
+++ features/base.feature
@@ -0,0 +1,4 @@
+Feature: Base
+  Scenario: Site work
+    Given I am on the homepage
+    Then I should see "Laravel Québec!"
---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,15 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext
+{
+
+}
