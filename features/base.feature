7baa71e0e360e0efbf2b6bc9f381ba459f219caf
Marc-Antoine Bouchard Marceau <marcantoine.bouchardm@gmail.com>
2015-09-13 18:10:28 -0400

    Start the form to sign in and sign up

--- features/base.feature
+++ features/base.feature
@@ -2,3 +2,16 @@ Feature: Base
   Scenario: Site work
     Given I am on the homepage
     Then I should see "Laravel Québec!"
+
+  Scenario: The sign in form is on the homepage
+    Given I am on the homepage
+    Then I should see "Connexion"
+    And I should see "Courriel" in the "div.signin" element
+    And I should see "Mot de passe" in the "div.signin" element
+    And I should see an "input[type='submit']" element
+
+  Scenario: The sign up form is on the homepage
+    Given I am on the homepage
+    Then I should see "Inscription"
+    And I should see "Courriel" in the "div.signup" element
+    And I should see an "input[type='submit']" element
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,4 +2,26 @@
 
 @section('content')
     <h1>Laravel Québec!</h1>
+
+    <div class="signin">
+        <h2>Connexion</h2>
+        <form action="">
+            <label for="email">Courriel:</label>
+            <input type="email" name="email" id="email" />
+
+            <label for="password">Mot de passe:</label>
+            <input type="password" name="password" id="password" />
+
+            <input type="submit" value="Connexion"/>
+        </form>
+    </div>
+
+    <div class="signup">
+        <h2>Inscription</h2>
+
+        <label for="email">Courriel:</label>
+        <input type="email" name="email" id="email" />
+
+        <input type="submit" value="Inscription"/>
+    </div>
 @endsection
